9f274aea55512e1d1e32aa49cee257d396c6e92f
alex petrenko <klim2041@yandex.ru>
2017-06-25 22:39:55 +0500

    Добавил вывод подробного описания по фигуре

--- FigureWpfApp/Figures/FigureBase.cs
+++ FigureWpfApp/Figures/FigureBase.cs
@@ -5,5 +5,6 @@
         public abstract FigureType Type { get; }
         public abstract double Perimeter { get; }
         public string Name { get { return Type.GetDescription(); } }
+        public string TypeName { get { return Type.GetDescription(); } }
     }
 }
